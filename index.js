c0951f817c129014672999d0187989a1ae1a0170
hkl1014 <linghangking93@hotmail.com>
2017-09-10 23:26:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,3 +7,9 @@ function updateObjectWithKeyAndValue(object,key,value){
 function destructivelyUpdateObjectWithKeyAndValue(object,key,value){
   return Object.assign(object,{[key]:value})
 }
+
+function deleteFromObjectByKey(object,key){
+  var a = Object.assign({},object)
+  delete a[key]
+  return a
+}
